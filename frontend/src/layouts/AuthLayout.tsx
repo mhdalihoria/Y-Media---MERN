de0b63cf2e512bfa47016745bdab3a1e7d3617d7
mhdalihoria <alihoria123456@gmail.com>
2025-02-04 16:27:56 +0300

    Styling AuthLayout

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,9 +1,29 @@
+import { Box, Container, Paper, styled } from "@mui/material";
 import { Outlet } from "react-router";
+
+const ParentContainerStyled = styled(Box)(({ theme }) => ({
+  minHeight: "100vh",
+  minWidth: "100vw",
+  background: theme.palette.background.default,
+  margin: 0,
+  padding: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const ChildContainerStyled = styled(Container)(({ theme }) => ({
+  background: theme.palette.background.paper,
+  padding: "4rem",
+  borderRadius: "15px"
+}));
 
 export default function AuthLayout() {
   return (
-    <>
-      <Outlet></Outlet>
-    </>
+    <ParentContainerStyled>
+      <ChildContainerStyled maxWidth="xs">
+        <Outlet></Outlet>
+      </ChildContainerStyled>
+    </ParentContainerStyled>
   );
 }
